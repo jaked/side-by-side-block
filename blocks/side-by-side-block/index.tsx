d6b0c38b2f843faa86add068a6572cfdb4684f28
Jake Donham <jaked@github.com>
2022-06-23 16:39:09 -0700

    avoid rerendering BlockComponents (?)

--- blocks/side-by-side-block/index.tsx
+++ blocks/side-by-side-block/index.tsx
@@ -1,30 +1,36 @@
+import { useMemo } from "react";
 import { FileBlockProps } from "@githubnext/blocks";
 
 export default function (props: FileBlockProps) {
   const { context, BlockComponent } = props;
 
-  return (
-    <div style={{ display: "grid" }}>
-      <div>
-        <BlockComponent
-          block={{
-            owner: "githubnext",
-            repo: "blocks-examples",
-            id: "code-block",
-          }}
-          context={context}
-        />
+  const rendered = useMemo(
+    () => (
+      <div style={{ display: "grid" }}>
+        <div>
+          <BlockComponent
+            block={{
+              owner: "githubnext",
+              repo: "blocks-examples",
+              id: "code-block",
+            }}
+            context={context}
+          />
+        </div>
+        <div>
+          <BlockComponent
+            block={{
+              owner: "githubnext",
+              repo: "blocks-examples",
+              id: "markdown-block",
+            }}
+            context={context}
+          />
+        </div>
       </div>
-      <div>
-        <BlockComponent
-          block={{
-            owner: "githubnext",
-            repo: "blocks-examples",
-            id: "markdown-block",
-          }}
-          context={context}
-        />
-      </div>
-    </div>
+    ),
+    []
   );
+
+  return rendered;
 }
