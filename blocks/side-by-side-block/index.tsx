79a69084210e0cb807154a482ab50c51dd758353
Jake Donham <jaked@github.com>
2022-06-24 10:34:29 -0700

    maybe rerendering BlockComponents is OK?

--- blocks/side-by-side-block/index.tsx
+++ blocks/side-by-side-block/index.tsx
@@ -4,39 +4,34 @@ import { FileBlockProps } from "@githubnext/blocks";
 export default function (props: FileBlockProps) {
   const { context, BlockComponent } = props;
 
-  const rendered = useMemo(
-    () => (
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          height: "100%",
-        }}
-      >
-        <div style={{ borderRight: "solid 1px rgba(225, 228, 232)" }}>
-          <BlockComponent
-            block={{
-              owner: "githubnext",
-              repo: "blocks-examples",
-              id: "code-block",
-            }}
-            context={context}
-          />
-        </div>
-        <div>
-          <BlockComponent
-            block={{
-              owner: "githubnext",
-              repo: "blocks-examples",
-              id: "markdown-block",
-            }}
-            context={context}
-          />
-        </div>
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        height: "100%",
+      }}
+    >
+      <div style={{ borderRight: "solid 1px rgba(225, 228, 232)" }}>
+        <BlockComponent
+          block={{
+            owner: "githubnext",
+            repo: "blocks-examples",
+            id: "code-block",
+          }}
+          context={context}
+        />
       </div>
-    ),
-    []
+      <div>
+        <BlockComponent
+          block={{
+            owner: "githubnext",
+            repo: "blocks-examples",
+            id: "markdown-block",
+          }}
+          context={context}
+        />
+      </div>
+    </div>
   );
-
-  return rendered;
 }
